3c5bd304edb9f709c00ef7500df0286bc49ba11b
RocketMan112 <ckschanker@gmail.com>
2017-11-30 15:49:20 -0700

    Merger

--- Varibles.c
+++ Varibles.c
@@ -1,27 +1,32 @@
 int X1=0,X2=0, Y1=0,Y2=0, threshold=15;
 int cycle = 0;
+int LeftMotorEncoder;
+int RightMotorEncoder;
+int Stage1MotorEncoder;
+int CenterValue = 1965;
+int ArmPostion = 0;
 int Stage1AutoPos = 0;
 
 int LeftBaseSpeed = 0;
-signed char BaseLeftArray [175];
+//signed char BaseLeftArray [175];
 
 int RightBaseSpeed = 0;
-signed char BaseRightArray [175];
+//signed char BaseRightArray [175];
 
 int Stage1LeftSpeed = 0;
-signed char Stage1LeftArray [175];
+//signed char Stage1LeftArray [175];
 
 int Stage1RightSpeed = 0;
-signed char Stage1RightArray [175];
+//signed char Stage1RightArray [175];
 
 int Stage2Speed = 0;
-signed char Stage2Array [175];
+//signed char Stage2Array [175];
 
 int GrabberSpeed = 0;
-signed char GrabberArray [175];
+//signed char GrabberArray [175];
 
 int TorqueLeftSpeed = 0;
-signed char TorqueLeftArray [175];
+//signed char TorqueLeftArray [175];
 
 int TorqueRightSpeed = 0;
-signed char TorqueRightArray [175];
+//signed char TorqueRightArray [175];
